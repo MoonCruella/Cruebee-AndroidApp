b5d40a4fd386204b58b96398563f352a049c4ca6
MunCruella <nhunguyetpy206@gmail.com>
2025-03-27 15:12:17 +0700

    Mot so chuc nang trong phan showmore

---
+++ app/src/main/java/com/example/project/PolicyActivity.java
@@ -0,0 +1,27 @@
+package com.example.project;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.white, getTheme()));
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.white, getTheme()));
+            int flags = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+        }
+        setContentView(R.layout.activity_policy);
+
+    }
+}
