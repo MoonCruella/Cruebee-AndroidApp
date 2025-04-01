92670cf8293cc7f6a38a01581f5a3bd6f7cff6d7
MunCruella <nhunguyetpy206@gmail.com>
2025-04-02 00:53:17 +0700

    Fix sau merge

--- app/src/main/java/com/example/project/interfaces/TotalFeeResponse.java
+++ app/src/main/java/com/example/project/interfaces/TotalFeeResponse.java
@@ -1,6 +1,8 @@
 package com.example.project.interfaces;
 
+import org.json.JSONException;
+
 public interface TotalFeeResponse {
-    void onSuccess(int totalFee);
+    void onSuccess(int totalFee) throws JSONException;
     void onError(String errorMessage);
 }
